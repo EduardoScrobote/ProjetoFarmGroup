42e874ead20c3b0a6f7a0c9c635af1c7e548e271
arturMucci <artur.mucci@gmail.com>
2023-08-02 11:43:54 -0300

    fix(main): commiting branch merge changes fixed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,8 +4,7 @@ export default function App() {
 
   return (
     <>
-      {/* <h1 className='text-xl text-red-200'>TEST</h1> */}
-      <h1>TEXTÂO BRABO DE TESTE</h1>
+      <h1 className='text-xl text-red-200'>TEST</h1>
     </>
   )
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,13 +5,7 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-<<<<<<< HEAD:frontend/src/main.tsx
-  <App />,
-=======
-  <React.StrictMode>
-    <BrowserRouter>
+  <BrowserRouter>
     <App />
-    </BrowserRouter>
-  </React.StrictMode>,
->>>>>>> 3016e9760ebf4a14a1a0bcbf1da3a82286a5b7df:frontEnd/src/main.tsx
+  </BrowserRouter>,
 )
