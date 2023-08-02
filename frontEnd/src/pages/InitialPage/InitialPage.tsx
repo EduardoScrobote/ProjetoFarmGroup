68eb186b74d82b351ac0238352c7ba7afd9412ca
Eduardo Scrobote <EduardoScroboteProfissional@gmail.com>
2023-08-02 14:45:16 -0300

    fix(LoginPage/RegisterPage): refatoring: UI/Design

--- frontEnd/src/pages/InitialPage/InitialPage.tsx
+++ frontEnd/src/pages/InitialPage/InitialPage.tsx
@@ -1,64 +1,32 @@
+import React from "react";
+
 import Sidebar from "../../components/Sidebar/Sidebar";
+
 function InitialPage() {
     return (
-        <div className="bg-green-600">
-            <div className="flex align-between justify-end">
-            <Sidebar />
-            </div>
-            <div className="flex grid grid-row">
-                <div className="flex">
-            <h1 className="text-4xl ml-12 mt-4">Bem vindo a Fazenda X</h1>
-                </div>
-                <div>
-            <p className="flex ml-28 w-1/5 mb-12 mt-12">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin fringilla
-                porta pharetra. Ut at mauris aliquam, elementum nibh sit amet, porta
-                ante. Nunc finibus at dolor et gravida. Duis ultrices ornare libero, eu
-                sollicitudin ipsum rutrum ac. Curabitur ante turpis, aliquet id tellus
-                sed, fringilla ornare diam. Curabitur arcu sapien, egestas vitae leo et,
-                facilisis malesuada odio. Interdum et malesuada fames ac ante ipsum
-                primis in faucibus. In pulvinar, tortor sit amet porttitor scelerisque,
-                tortor tellus elementum felis, sed auctor diam lectus sed elit. Ut ut
-                nulla et neque vulputate auctor. Vivamus imperdiet ex nec molestie
-                varius. Sed a est tristique, porttitor sem nec, feugiat elit. Integer
-                faucibus iaculis nibh, vel congue dui facilisis eget. Ut tincidunt
-                imperdiet augue vel venenatis. Sed id dui erat. Pellentesque consequat
-                accumsan malesuada. Integer id viverra lacus. In sit amet nisi ligula.
-                Donec lorem lacus, venenatis eu egestas non, scelerisque sit amet purus.
-                Suspendisse potenti. Aenean commodo augue in ligula condimentum
-                condimentum. Morbi quam urna, imperdiet vitae rhoncus et, pretium quis
-                neque. In bibendum ut libero non cursus. Orci varius natoque penatibus
-                et magnis dis parturient montes, nascetur ridiculus mus. Donec facilisis
-                varius nunc nec bibendum. Cras tortor libero, scelerisque ac varius eu,
-                molestie vitae erat.
-            </p>
-            <div className="flex justify-end mr-28">
-            <p className="w-1/5 mt-12">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin fringilla
-                porta pharetra. Ut at mauris aliquam, elementum nibh sit amet, porta
-                ante. Nunc finibus at dolor et gravida. Duis ultrices ornare libero, eu
-                sollicitudin ipsum rutrum ac. Curabitur ante turpis, aliquet id tellus
-                sed, fringilla ornare diam. Curabitur arcu sapien, egestas vitae leo et,
-                facilisis malesuada odio. Interdum et malesuada fames ac ante ipsum
-                primis in faucibus. In pulvinar, tortor sit amet porttitor scelerisque,
-                tortor tellus elementum felis, sed auctor diam lectus sed elit. Ut ut
-                nulla et neque vulputate auctor. Vivamus imperdiet ex nec molestie
-                varius. Sed a est tristique, porttitor sem nec, feugiat elit. Integer
-                faucibus iaculis nibh, vel congue dui facilisis eget. Ut tincidunt
-                imperdiet augue vel venenatis. Sed id dui erat. Pellentesque consequat
-                accumsan malesuada. Integer id viverra lacus. In sit amet nisi ligula.
-                Donec lorem lacus, venenatis eu egestas non, scelerisque sit amet purus.
-                Suspendisse potenti. Aenean commodo augue in ligula condimentum
-                condimentum. Morbi quam urna, imperdiet vitae rhoncus et, pretium quis
-                neque. In bibendum ut libero non cursus. Orci varius natoque penatibus
-                et magnis dis parturient montes, nascetur ridiculus mus. Donec facilisis
-                varius nunc nec bibendum. Cras tortor libero, scelerisque ac varius eu,
-                molestie vitae erat.
-            </p>
-            </div>
+            <div className="bg-[url('../../../public/images/bg-initial.jpg')] w-screen h-screen">
+                <div className="flex align-between justify-end">
+                        <Sidebar />
+                    </div>
+                            <h1 className="flex text-4xl ml-12">Bem vindo a Fazenda X</h1>
+                    <div className="flex justify-center">
+                        <div className="w-2/4">
+                            <p className="flex text-2xl leading-relaxed">
+                                O Projeto Fazenda X é um empreendimento em grupo com o principal
+                                objetivo de desenvolver as habilidades full-stack da equipe de
+                                desenvolvimento. Trata-se de um grupo de desenvolvedores que
+                                estão no início de suas carreiras, trabalhando na criação de uma
+                                aplicação web para soluções agrícolas. Nossa equipe se esforça
+                                para oferecer o melhor em cada funcionalidade e interação com o
+                                usuário, buscando também aprimorar nossas habilidades técnicas,
+                                trabalho em equipe, versionamento de código e a aplicação de
+                                metodologias ágeis, como o Kanban. Estamos empenhados em evoluir
+                                tanto individualmente quanto coletivamente para construir uma
+                                plataforma de qualidade.
+                            </p>
+                        </div>
                 </div>
             </div>
-        </div>
     );
 }
 
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,23 @@
-import './App.css'
+import React from 'react';
+import {Routes, Route} from 'react-router-dom'
 
-export default function App() {
+import InitialPage from './pages/InitialPage/InitialPage'
+import LoginPage from './pages/LogingPage/LoginPage';
+import RegisterPage from './pages/RegisterPage/RegisterPage';
+
+import './App.css';
+
+function App () {
 
   return (
     <>
-      <h1 className='text-xl text-red-200'>TEST</h1>
+    <Routes>
+      <Route path='/' element={<InitialPage />}/>
+      <Route path='/login' element={<LoginPage />}/>
+      <Route path='/login/register' element={<RegisterPage />} />
+    </Routes>
     </>
   )
 }
+
+export default App;
