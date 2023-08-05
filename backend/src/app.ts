9e3bceef1a4fb01479e192f7fc484425bd05133d
arturMucci <artur.mucci@gmail.com>
2023-08-05 17:11:51 -0300

    feature(backend): docker network

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,7 +3,7 @@ import express, { Request, Response } from 'express';
 const app = express();
 
 app.get('/', (_req: Request, res: Response) => {
-  return res.status(200).json({ message: "Deu bão!" });
+  return res.status(200).json({ message: "Hello world!" });
 });
 
 export default app;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,5 +3,5 @@ import app from './app';
 const port = process.env.EXPRESS_APP_PORT || 3001;
 
 app.listen(port, () => {
-  console.log(`Tchuplek tchuplim no ratiofly na porta: ${port}`);
+  console.log(`Server running on port: ${port}`);
 });
